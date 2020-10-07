8615a25469743d33cf9b83eacf6b96eb754e91c5
Pankaj Rayal <https://github.dxc.com/>
2020-10-07 16:08:36 +0530

    Inital Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleASPNETCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
